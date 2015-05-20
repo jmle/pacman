1e20ef0295b26926324ba1c13a54eae9e78d1ddf
qatsindio <jmleflet@gmail.com>
2015-05-20 23:14:15 +0100

    Adding untracked files

---
+++ Assets/Scripts/Ghost/GhostHouseHandler.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handles the ghost when entering and exiting the house.
+/// </summary>
+public class GhostHouseHandler : MonoBehaviour {
+
+	private GhostStateManager ghostStateManager;
+
+	// Use this for initialization
+	void Start () {
+		ghostStateManager = GetComponent<GhostStateManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (ghostStateManager.GetGhostState()) {
+		case GhostState.EXIT:
+			HandleHouseExit ();
+			break;
+		case GhostState.DEAD:
+			HandleHouseEnter ();
+			break;
+		case GhostState.ENTER:
+			HandleHouseInside ();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void HandleHouseExit () {
+		if (HasArrived (GlobalPositions.HOUSE_DOOR)) {
+			ghostStateManager.SetGhostStateToCurrentGlobalState ();
+		}
+	}
+
+	private void HandleHouseEnter () {
+		if (HasArrived (GlobalPositions.HOUSE_DOOR)) {
+			ghostStateManager.SetGhostState (GhostState.ENTER);
+		}
+	}
+
+	private void HandleHouseInside () {
+		if (HasArrived (GlobalPositions.HOUSE_MIDDLE)) {
+			// If the ghost was dead, he goes out immediately (not actually true)
+			// TODO: This depends on more things, but for now it's fine
+			ghostStateManager.SetGhostState (GhostState.EXIT);
+		}
+	}
+
+	private bool HasArrived (Vector2 position) {
+		return Vector2.Distance (rigidbody2D.position, position) < 0.15;
+	}
+}
---
+++ Assets/Scripts/GlobalPositions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple class to hold static references to key positions in the scenario.
+/// </summary>
+public class GlobalPositions  {
+
+	public static Vector2 HOUSE_DOOR = new Vector2 (13.5f, 19);
+	public static Vector2 HOUSE_MIDDLE = new Vector2 (13.5f, 16);
+
+
+}
