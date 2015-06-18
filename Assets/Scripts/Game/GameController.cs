1e4164fdbeec8d54c046e4bc4dde9cb306764257
qatsindio <jmleflet@gmail.com>
2015-06-19 00:41:00 +0100

    Adding restart level logic

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -4,18 +4,42 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 
 	public int livesLeft;
+	public float timeToRestart;
 
 	public PacmanController pacman;
 	public GhostManager ghosts;
 
+	private bool restartLevel;
+	private float restartCounter;
+
 	// Use this for initialization
 	void Start () {
-
+		restartCounter = timeToRestart;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (restartLevel) {
+			if (restartCounter <= 0) {
+				restartLevel = false;
+				restartCounter = timeToRestart;
 
+				RestartLevel ();
+			} else {
+				restartCounter -= Time.deltaTime;
+			}
+		}
+	}
+
+	private void RestartLevel () {
+		livesLeft--;
+
+		if (livesLeft == 0) {
+			// TODO: game over
+		} else {
+//			Application.LoadLevel (0);
+			Debug.Log ("Restarting level...");
+		}
 	}
 
 	/// <summary>
@@ -24,5 +48,7 @@ public class GameController : MonoBehaviour {
 	public void Die () {
 		ghosts.FreezeGhosts ();
 		pacman.Freeze ();
+
+		restartLevel = true;
 	}
 }
